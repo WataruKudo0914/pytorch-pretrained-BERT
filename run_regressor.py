5fda1fa5456b1cb6a6722d8f316ed640d1373d3f
Wataru Kudo <wataru-kudo048@g.ecc.u-tokyo.ac.jp>
2018-12-01 19:40:42 +0900

    fix bug

--- run_regressor.py
+++ run_regressor.py
@@ -221,8 +221,8 @@ class ArdProcessor(DataProcessor):
     examples = []
     for (i, line) in enumerate(lines):
       guid = "%s-%s" % (set_type, i)
-      text_a = tokenization.convert_to_unicode(line[0])
-      label = tokenization.convert_to_unicode(line[1])
+      text_a = line[0]
+      label = line[1]
       examples.append(
           InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
     return examples
