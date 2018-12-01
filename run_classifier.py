91dfb3494b1007ca52495648238b7c3024458536
Wataru Kudo <wataru-kudo048@g.ecc.u-tokyo.ac.jp>
2018-12-01 22:31:50 +0900

    edit ArdProcessor _read_tsv

--- run_classifier.py
+++ run_classifier.py
@@ -196,26 +196,26 @@ class ArdProcessor(DataProcessor):
   """Amazon Review Dataset"""
   """Code from https://qiita.com/sugiyamath/items/9ddba6a858e68968d929 """
 
-  def read_tsv(self, path):
-    df = pd.read_csv(path, sep="\t")
-    return [(str(text), str(label)) for text,label in zip(df['reviewText'],
-                                                          df['helpful_bin'])]
+  # def read_tsv(self, path):
+  #   df = pd.read_csv(path, sep="\t")
+  #   return [(str(text), str(label)) for text,label in zip(df['reviewText'],
+  #                                                         df['helpful_bin'])]
 
 
   def get_train_examples(self, data_dir):
     """See base class."""
     return self._create_examples(
-        self.read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+        self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
 
   def get_dev_examples(self, data_dir):
     """See base class."""
     return self._create_examples(
-        self.read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+        self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
 
   def get_test_examples(self, data_dir):
     """See base class."""
     return self._create_examples(
-      self.read_tsv(os.path.join(data_dir, "test.tsv")), "test")
+      self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
 
   def get_labels(self):
     """See base class."""
@@ -227,7 +227,7 @@ class ArdProcessor(DataProcessor):
     for (i, line) in enumerate(lines):
       guid = "%s-%s" % (set_type, i)
       text_a = line[0]
-      label = line[1]
+      label = line[2]
       examples.append(
           InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
     return examples
